a30a6f1ce0e951d13adb329bc5cc31773644b8cc
Geeta Patil <geetarpatil36@gmail.com>
2022-10-12 21:30:45 +0000

    Java file added

---
+++ ArrayList1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ArrayList1
+{
+public static void main(String[] args)
+{
+List<String> li=new ArrayList<String>();
+li.add("KM");
+li.add("GM");
+li.add("RB");
+li.add("SM");
+
+Iterator itr=li.iterator();
+
+while(itr.hasNext())
+{
+System.out.println(itr.next());
+  }
+}
+}
