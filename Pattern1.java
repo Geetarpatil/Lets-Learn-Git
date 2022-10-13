834c2e6c92568ab5c4d6da5c404052d9f2f55999
Geeta Patil <geetarpatil36@gmail.com>
2022-10-13 12:02:22 +0000

    added pattern.java

---
+++ Pattern1.java
@@ -0,0 +1,18 @@
+class Pattern1
+{
+public static void main(String[] args)
+{
+ for(int i=0;i<5;i++)
+ {
+	System.out.println(); //for next line
+
+    	for(int j=0;j<=i;j++)
+	{
+	  System.out.print(" *");
+	}
+System.out.println(" ");
+}
+
+}
+}
+ 
